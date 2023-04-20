8769707fdd9a144a1841ef1668e60448266fc713
Lenovo <Lenovo@DESKTOP-SUK2R0S>
2023-04-20 16:13:34 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the cost of notebook:");
+            int notebook = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the cost of pen:");
+            int pen = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number of notebook");
+            int notebookBuy = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the cost of pen:");
+            int penBuy = Convert.ToInt32(Console.ReadLine());
+            int sum = notebook * notebookBuy + pen * penBuy;
+            Console.WriteLine($"Sum= {sum}");
+            Console.ReadLine();
+        }
+    }
+}
